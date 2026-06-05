83823399f4362ae8236400a874ad1bf8587b6cbd
MiAl-Jr <mithunbiju992@gmail.com>
2026-06-05 12:24:23 +0530

    New update

--- server.js
+++ server.js
@@ -1,31 +1,37 @@
 const express = require("express");
-const app = express();
+const http = require("http");
+const { Server } = require("socket.io");
 
-const http = require("http").createServer(app);
-const io = require("socket.io")(http);
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 
 app.use(express.static("public"));
 
 io.on("connection", (socket) => {
-    socket.on("ruk ja likh rha hu!!", () => {
-           socket.broadcast.emit("ruk ja likh rha hu!!");
-    });
+    console.log("User connected:", socket.id);
 
-    console.log("User connected");
-    socket.on("typing", () => {
-
-        socket.broadcast.emit("typing");
-
-    });
-
+    // chat message
     socket.on("chat message", (msg) => {
-
         io.emit("chat message", msg);
-
     });
 
+    // typing event
+    socket.on("typing", (name) => {
+        socket.broadcast.emit("typing", name);
+    });
+
+    socket.on("stop typing", () => {
+        socket.broadcast.emit("stop typing");
+    });
+
+    socket.on("disconnect", () => {
+        console.log("User disconnected");
+    });
 });
 
-http.listen(3000, () => {
-    console.log("Server running");
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+    console.log("Server running on port " + PORT);
 });
