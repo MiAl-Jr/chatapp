0bdb2ef6c0498aeaf5d773ac804910f93c4c8629
MiAl-Jr <mithunbiju992@gmail.com>
2026-06-05 13:05:28 +0530

    ONLine UPdate

--- server.js
+++ server.js
@@ -8,25 +8,36 @@ const io = new Server(server);
 
 app.use(express.static("public"));
 
+let users = {}; // socket.id -> name
+
 io.on("connection", (socket) => {
     console.log("User connected:", socket.id);
+
+    // when user joins
+    socket.on("join", (name) => {
+        users[socket.id] = name;
+
+        io.emit("user list", Object.values(users)); // update all users
+    });
 
     // chat message
     socket.on("chat message", (msg) => {
         io.emit("chat message", msg);
     });
 
-    // typing event (send user name + id)
-    socket.on("typing", (data) => {
-        socket.broadcast.emit("typing", data);
+    // typing
+    socket.on("typing", (name) => {
+        socket.broadcast.emit("typing", name);
     });
 
-    socket.on("stop typing", (data) => {
-        socket.broadcast.emit("stop typing", data);
+    socket.on("stop typing", (name) => {
+        socket.broadcast.emit("stop typing", name);
     });
 
+    // disconnect = offline
     socket.on("disconnect", () => {
-        console.log("User disconnected:", socket.id);
+        delete users[socket.id];
+        io.emit("user list", Object.values(users));
     });
 });
 
