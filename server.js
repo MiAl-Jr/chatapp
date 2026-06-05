69f718bf2ff18025ba63d8ed14714fbd46aaab6f
MiAl-Jr <mithunbiju992@gmail.com>
2026-06-05 11:20:54 +0530

    mongodb added

--- server.js
+++ server.js
@@ -4,16 +4,56 @@ const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 
+const mongoose = require("mongoose");
+
 app.use(express.static("public"));
 
-io.on("connection", (socket) => {
-    console.log("A user connected");
+mongoose.connect("mongodb+srv://MiAl-Jr:9205441522@mial-jr.op7nak8.mongodb.net/?appName=MiAl-Jr");
 
-    socket.on("chat message", (msg) => {
+const MessageSchema = new mongoose.Schema({
+    text:String
+});
+
+const Message = mongoose.model("Message", MessageSchema);
+
+let onlineUsers = 0;
+
+io.on("connection", async (socket) => {
+
+    console.log("User connected");
+
+    onlineUsers++;
+
+    io.emit("online users", onlineUsers);
+
+    const oldMessages = await Message.find();
+
+    socket.emit("load messages", oldMessages);
+
+    socket.on("chat message", async (msg) => {
+
+        const message = new Message({
+            text:msg
+        });
+
+        await message.save();
+
         io.emit("chat message", msg);
+
     });
+
+    socket.on("disconnect", () => {
+
+        onlineUsers--;
+
+        io.emit("online users", onlineUsers);
+
+        console.log("User disconnected");
+
+    });
+
 });
 
 http.listen(3000, () => {
-    console.log("Server running on port 3000");
+    console.log("Server running");
 });
