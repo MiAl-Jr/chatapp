d2b88e80a059571e97c2fa8bc5dd2740acccab67
MiAl-Jr <mithunbiju992@gmail.com>
2026-06-05 11:41:58 +0530

    removed mongodb

--- server.js
+++ server.js
@@ -4,51 +4,15 @@ const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 
-const mongoose = require("mongoose");
-
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://MiAl-Jr:9205441522@mial-jr.op7nak8.mongodb.net/?appName=MiAl-Jr");
-
-const MessageSchema = new mongoose.Schema({
-    text:String
-});
-
-const Message = mongoose.model("Message", MessageSchema);
-
-let onlineUsers = 0;
-
-io.on("connection", async (socket) => {
+io.on("connection", (socket) => {
 
     console.log("User connected");
 
-    onlineUsers++;
-
-    io.emit("online users", onlineUsers);
-
-    const oldMessages = await Message.find();
-
-    socket.emit("load messages", oldMessages);
-
-    socket.on("chat message", async (msg) => {
-
-        const message = new Message({
-            text:msg
-        });
-
-        await message.save();
+    socket.on("chat message", (msg) => {
 
         io.emit("chat message", msg);
-
-    });
-
-    socket.on("disconnect", () => {
-
-        onlineUsers--;
-
-        io.emit("online users", onlineUsers);
-
-        console.log("User disconnected");
 
     });
 
